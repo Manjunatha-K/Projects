8c46b817abc24b766a26351d989ddbfde768d478
Manjunatha <k.manjunatha1518@gmail.com>
2025-08-04 07:22:54 +0530

    Exam Portal

---
+++ Exam Portal/ExamServer/src/test/java/com/portal/ExamServer/ExamServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portal.ExamServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
